98f9ade7490d7442b1fe9b9cfba57ee42d045cfe
dulzzzz <abdultwx@gmail.com>
2023-09-25 20:35:36 +0700

    commit

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -2,11 +2,8 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Contoh1 extends CI_Controller {
-
-	
-	 */
 	public function index()
 	{
-		echo "<h1>Bangsat</h1>"; 
+		echo "<h1>Test</h1>"; 
 	}
 }
--- application/views/view-latihan1.php
+++ application/views/view-latihan1.php
@@ -1,12 +1,15 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
- <title>Latihan 1</title>
- </head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 <body>
- Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+Halo Kawan.. Yuk kita belajar web programming..!!!<br>
  Nilai 1 = <?= $nilai1; ?>
  Nilai 2 = <?= $nilai2; ?>
- ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
-$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+ ini hasil dari pemodelan dengan methode penjumlahan yaitu
+<?php $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
 </body>
 </html>
